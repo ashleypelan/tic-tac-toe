13c4524898dce9d87d2035c1d1bb2d91e12b18fc
Ashley Pelan <ashley.pelan@gmail.com>
2015-07-10 14:15:43 -0600

    stuff.

--- index.js
+++ index.js
@@ -2,22 +2,27 @@
 window.onload=function() {
   var array = [" X ", " O ", " X ", " O ", " X ", " O ", " X ", " O ", " X "];
 
+  //Create my gameboard!
   for (var i = 1; i < 10; i++) {
     var box = document.createElement("div");
     document.body.appendChild(box);
     box.className = ("box" + i);
 
+    if (i === 4 || i === 7 || i === 10) {
+      box.style.clear = "left";
+    }
+  // console.log(box);
+  }
+
+  //Make my buttons
+  for (var j = 0; j < array.length; j++) {
+  }
     var counter = 0;
     box.addEventListener("click", function() {
       counter += 1;
-      console.log(array[counter - 1]);
-      });
-
-
-
-    if (i === 4 || i === 7 || i === 10) {
-      box.style.clear = "left";
-
-    }
-  }
+      box.innerHTML = (array[counter - 1]);
+      if (counter > array.length){
+        alert("Draw! Please start over");
+      }
+    });
 }

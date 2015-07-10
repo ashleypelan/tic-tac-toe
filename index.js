b03583b6b5983a2598c54fa41eecd0d3d73f7e6a
Ashley Pelan <ashley.pelan@gmail.com>
2015-07-10 11:35:39 -0600

    board working and event listeners listening

--- index.js
+++ index.js
@@ -9,17 +9,12 @@ window.onload=function() {
   for (var i = 1; i < 10; i++) {
     var box=document.createElement("div");
     document.body.appendChild(box);
-    box.className="box";
+    box.className = ("box" + i);
 
-    if (i > 0 && i < 4) {
-      box.style.backgroundColor = "green";
-    }
-    if (i > 3 && i < 7) {
-      box.style.backgroundColor = "blue";
-    }
-    if (i > 6 && i < 10) {
-      box.style.backgroundColor = "yellow";
-    }
+    box.addEventListener("click", function() {
+      alert("you got it!");
+    });
+
     if (i === 4 || i === 7 || i === 10) {
       box.style.clear = "left";
 

c4f6c7d1bc7443d6abc8c08accdbc7531d669e7c
Ashley Pelan <ashley.pelan@gmail.com>
2015-07-10 11:17:50 -0600

    board divs populated

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-// for (var i = 1; i < 10; i++) {
-//   // boxes.setAttribute("id", "boxes" + i);
-//   var boxes = document.createElement("div");
-//   boxes.className = "boxes";
-//   document.getElementByID("board");
-//   board.appendChild(boxes);
-//
+// window.onLoad = function() {
+//   for (var i = 1; i < 10; i++) {
+//    var box = document.createElement("div");
+//    document.body.appendChild(box);
+//    box.className = "boxClassName";
+//   }
 // }
+window.onload=function() {
+  for (var i = 1; i < 10; i++) {
+    var box=document.createElement("div");
+    document.body.appendChild(box);
+    box.className="box";
 
+    if (i > 0 && i < 4) {
+      box.style.backgroundColor = "green";
+    }
+    if (i > 3 && i < 7) {
+      box.style.backgroundColor = "blue";
+    }
+    if (i > 6 && i < 10) {
+      box.style.backgroundColor = "yellow";
+    }
+    if (i === 4 || i === 7 || i === 10) {
+      box.style.clear = "left";
 
-var clickedBox;
-var box = document.getElementsByClassName("box");
-for (var i = 1; i < 10; i++) {
-  var boxDiv =  document.createElement("div");
-  boxDiv.className = "box";
-  boxDiv.setAttribute("id", "box" + i);
-  boxDiv.style.margin = "50px";
-  boxDiv.style.position = "relative";
-
-  main.appendChild(boxDiv);
+    }
+  }
 }

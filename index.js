09f90ea33166a8a336268c333501388150b53ad1
Ashley Pelan <ashley.pelan@gmail.com>
2015-07-10 13:58:46 -0600

    clicks working

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// window.onLoad = function() {
-//   for (var i = 1; i < 10; i++) {
-//    var box = document.createElement("div");
-//    document.body.appendChild(box);
-//    box.className = "boxClassName";
-//   }
-// }
+
 window.onload=function() {
+  var array = [" X ", " O ", " X ", " O ", " X ", " O ", " X ", " O ", " X "];
+
   for (var i = 1; i < 10; i++) {
-    var box=document.createElement("div");
+    var box = document.createElement("div");
     document.body.appendChild(box);
     box.className = ("box" + i);
 
+    var counter = 0;
     box.addEventListener("click", function() {
-      alert("you got it!");
-    });
+      counter += 1;
+      console.log(array[counter - 1]);
+      });
+
+
 
     if (i === 4 || i === 7 || i === 10) {
       box.style.clear = "left";
